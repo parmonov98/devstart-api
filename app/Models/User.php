bc2fc99de8f4572fd1d5d451326cd19073359c71
abror9109 <34788921+abror9109@users.noreply.github.com>
2023-07-27 21:46:44 +0500

    Dashboard user statistics (#14)
    * Add moonshine admin panel package
    Create default admin by seeder
    Ability block any user from admin panel
    
    * Add confirmation to block/unblock users
    
    * Small design changes
    
    * Change Admin user model
    
    * Formatted with php cs-fixer
    
    * Comments have been added for migrations
    Unnecessary commented codes have been removed
    
    * Dashboard statistics users count
    
    * Fix: use comments, constants
    
    * Fix comment

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Collection;
 use Illuminate\Notifications\Notifiable;
+use App\Сonstants\User\UserTypeConstants;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -24,6 +25,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  * @property bool|null $is_admin
  * @property Collection|Skill[] $skills
  * @property SmsVerification $sms
+ *
+ * @method static Builder developer()
+ * @method static Builder ideaHolder()
  */
 class User extends Authenticatable
 {
@@ -50,5 +54,27 @@ class User extends Authenticatable
     public function skills(): BelongsToMany
     {
         return $this->belongsToMany(Skill::class, 'developer_skills_pivot');
+    }
+
+    /**
+     * Scope a query to only include
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeDeveloper($query): Builder
+    {
+        return $query->where('user_type', '=', UserTypeConstants::USER_TYPE_DEVELOPER);
+    }
+
+    /**
+     * Scope a query to only include
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeIdeaHolder($query): Builder
+    {
+        return $query->where('user_type', '=', UserTypeConstants::USER_TYPE_IDEA_HOLDER);
     }
 }
